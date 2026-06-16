44d849f4968385519ebc2c17251df54d638749fe
Tharasri <tharasri_23it58@kgkite.ac.in>
2026-06-16 16:30:21 +0530

    sorting

---
+++ DSA/List/Algorithms/Selection.java
@@ -0,0 +1,32 @@
+public class Selection {
+
+    public static void SelectionSort(int arr[]) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i+1; j < arr.length; j++) {
+
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = arr[j];
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {5, 4, 3, 2, 1};
+
+        SelectionSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
